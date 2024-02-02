c111b071076ba46bef5efe7ef0fc9b7df2aafd95
shresthasusan <shresthasusan44@gmail.com>
2024-02-02 19:16:28 +0545

    .

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template ,url_for,flash,redirect,request
 from . import app
 from .output import output
+from werkzeug.utils import secure_filename
 import os
 
 
@@ -15,11 +16,13 @@ def about():
 @app.route('/',methods=['POST','GET'])
 def predict():
     imagefile=request.files['imagefile']
-    image_path=os.path.join(app.root_path,'static\\images\\demo_imgs',imagefile.filename)
-    imagefile.save(image_path)
-    img="/images/demo_imgs/"+imagefile.filename
-    title,ingredients,recipe = output(image_path)
-    return render_template('predict.html',title=title,ingredients=ingredients,recipe=recipe,img=img)
+    filename = secure_filename(imagefile.filename)
+    # image_path=os.path.join(app.root_path,'static\\images\\demo_imgs',imagefile.filename)
+    # image_path = os.path.join(app.root_path, 'static', 'images', 'demo_imgs', filename)
+    # imagefile.save(image_path)
+    # img="/images/demo_imgs/"+imagefile.filename
+    title,ingredients,recipe = output(imagefile)
+    return render_template('predict.html',title=title,ingredients=ingredients,recipe=recipe,img=imagefile)
 
 @app.route('/<samplefoodname>')
 def predictsample(samplefoodname):
