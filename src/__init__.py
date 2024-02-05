cea006ff146d4d7d44cb71f0db1c631470aea3c0
shresthasusan <shresthasusan44@gmail.com>
2024-02-05 08:37:05 +0545

    database_connection

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+import secrets
 
 app = Flask(__name__,template_folder='Templates')
 
+#mySQL Configuration
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'recipelens'
+app.config['MYSQL_USER'] = 'recipe_user'
+
+mysql = MySQL(app)
 
 from . import routes
--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,72 @@
-from flask import render_template ,url_for,flash,redirect,request
+from flask import render_template ,url_for,flash,redirect,request,session
 from . import app
+from flask_mysqldb import MySQL
 from .output import output
 from werkzeug.utils import secure_filename
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
+
+mysql = MySQL(app)
 
 
-@app.route('/',methods=['GET'])
+# Login routes
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_input = request.form['password']
+
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT username, password FROM users WHERE username = %s", (username))
+        user = cur.fetchone()
+        cur.close()
+
+        if user and check_password_hash(user[1], password_input):
+            session['username'] = user[0]
+            flash('Login successful', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid username or password', 'error')
+            return render_template('login.html', error='Invalid username or password')
+
+    return render_template('login.html')
+
+# Registration Routes
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username already exists
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+        existing_user = cur.fetchone()
+        cur.close()
+
+        if existing_user:
+            flash('Username already exists. Please choose another username.', 'error')
+            return redirect(url_for('register'))
+
+        # Hash the password before storing it
+        hashed_password = generate_password_hash(password, method='sha256')
+
+        # Save 'username' and 'hashed_password' in the database
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, hashed_password))
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Registration successful. You can now log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('registration.html')
+
+# App routes
+  
+@app.route('/home',methods=['GET','POST'])
 def home():
     return render_template('home.html')
 
@@ -13,7 +74,7 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/predict',methods=['POST','GET'])
 
 def predict():
     imagefile = request.files['imagefile']
